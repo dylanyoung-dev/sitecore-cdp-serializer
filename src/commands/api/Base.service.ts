11a3e0be5173c87467d4f9242ddc1c0c55d3ab8d
Meng Hak <meng.hak@xcentium.com>
2022-10-03 13:25:57 -0700

    add base service

---
+++ src/commands/api/Base.service.ts
@@ -0,0 +1,58 @@
+/**
+ * Base service for all API calls
+ */
+
+ import Configstore from "configstore";
+ import { AuthToken } from './auth/Auth.interface.js';
+ import fetch from 'node-fetch';
+ 
+ const BaseService = (config: Configstore) => {
+     const serviceUrl = config.get('serviceUrl');
+     const credentials: AuthToken = config.get('credentials');
+     const version = "v3"; // todo: config option
+ 
+     const Fetch = async(method: string, path: string, body: string | null) => {
+         if (!serviceUrl) {
+             throw 'Service URL not set, re-run auth command';
+         }
+       
+         if (!credentials) {
+             throw 'You must run the auth command first to initialize the CLI';
+         }
+ 
+         return await fetch(`https://${serviceUrl}/${version}/${path}`, {
+             method,
+             body,
+             headers: {
+               Authorization: `Bearer ${credentials.access_token}`,
+               'Content-Type': 'application/json',
+             },
+         });
+     }
+ 
+     const Get = async(path: string) => {
+         return await Fetch("get", path, null);
+     }
+ 
+     const Post = async(path: string, body: string | null) => {
+         return await Fetch("post", path, body);
+     }
+ 
+     const Put = async(path: string, body: string | null) => {
+         return await Fetch("put", path, body);
+     }
+ 
+     const Delete = async(path: string, body: string | null) => {
+         return await Fetch("delete", path, body);
+     }
+ 
+     return {
+         Fetch,
+         Get,
+         Post,
+         Put,
+         Delete,
+     };
+ }
+ 
+ export { BaseService };
--- src/commands/api/templates/Template.service.ts
+++ src/commands/api/templates/Template.service.ts
@@ -1,143 +1,91 @@
 import Configstore from 'configstore';
 import fetch, { Response } from 'node-fetch';
 import chalk from 'chalk';
-import { AuthToken } from '../auth/Auth.interface.js';
 import { Command } from 'commander';
 import { Template } from './Template.interface.js';
 import { logline } from '../../../utils/index.js';
+import { BaseService } from '../Base.service.js'
 
 const TemplateService = (config: Configstore) => {
-  const serviceUrl = config.get('serviceUrl');
-  const credentials: AuthToken = config.get('credentials');
-
+  const baseService = BaseService(config);
+  
   const GetAllTemplates = async (templateType: string) => {
-    if (!serviceUrl) {
-      logline(chalk.red('Service URL not set, re-run auth command'));
-      return;
-    }
-
-    if (!credentials) {
-      logline(chalk.red('You must run the auth command first to initialize the CLI'));
-      return;
-    }
-
-    let servicePath = `https://${serviceUrl}/v3/templates`;
-
-    const response: Response = await fetch(servicePath, {
-      method: 'get',
-      body: null,
-      headers: {
-        Authorization: `Bearer ${credentials.access_token}`,
-      },
-    });
-
-    if (response.ok) {
-      logline(chalk.green('success'));
-      let templates: Template[] = (await response.json()) as Template[];
-
-      return templates;
-    } else {
-      console.log(chalk.red('Failed to retrieve templates'));
+    try {
+      const response: Response = await baseService.Get('templates');
+  
+      if (response.ok) {
+        logline(chalk.green('success'));
+        let templates: Template[] = (await response.json()) as Template[];
+  
+        return templates;
+      } else {
+        console.log(chalk.red('Failed to retrieve templates'));
+      }
+    } catch (ex) {
+      logline(chalk.red(ex));
     }
   };
 
   const GetByFriendlyId = async (friendlyId: string): Promise<Template | null> => {
-    if (!serviceUrl) {
-      logline(chalk.red('Service URL not set, re-run auth command'));
-      return null;
+    try {
+      const response: Response = await baseService.Get(`templates/${friendlyId}`);
+
+      if (response.ok) {
+        const result: Template = (await response.json()) as Template;
+
+        logline(chalk.green('Template retrieved successfully'));
+
+        return result;
+      } else {
+        logline(chalk.red('Failed to retrieve template'));
+        return null;
+      }
+    } catch (ex) {
+      logline(chalk.red(ex));
     }
 
-    if (!credentials) {
-      logline(chalk.red('You must run the auth command first to initialize the CLI'));
-      return null;
-    }
-
-    let servicePath = `https://${serviceUrl}/v3/templates/${friendlyId}`;
-
-    const response: Response = await fetch(servicePath, {
-      method: 'get',
-      body: null,
-      headers: {
-        Authorization: `Bearer ${credentials.access_token}`,
-      },
-    });
-
-    if (response.ok) {
-      const result: Template = (await response.json()) as Template;
-
-      logline(chalk.green('Template retrieved successfully'));
-
-      return result;
-    } else {
-      logline(chalk.red('Failed to retrieve template'));
-      return null;
-    }
+    return null;
   };
 
   const CreateTemplate = async (template: Template): Promise<Template | null> => {
-    if (!serviceUrl) {
-      logline(chalk.red('Service URL not set, re-run auth command'));
-      return null;
+
+    try {
+      const response: Response = await baseService.Post('templates', JSON.stringify(template));
+
+      if (response.ok) {
+        const result: Template = (await response.json()) as Template;
+
+        return result;
+      } else {
+        logline(chalk.red('Failed to create template'));
+        logline(JSON.stringify(await response.json()));
+        return null;
+      }
+    } catch (ex) {
+      logline(chalk.red(ex));
     }
-
-    if (!credentials) {
-      logline(chalk.red('You must run the auth command first to initialize the CLI'));
-      return null;
-    }
-
-    let servicePath = `https://${serviceUrl}/v3/templates`;
-
-    const response: Response = await fetch(servicePath, {
-      method: 'post',
-      body: JSON.stringify(template),
-      headers: {
-        Authorization: `Bearer ${credentials.access_token}`,
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.ok) {
-      const result: Template = (await response.json()) as Template;
-
-      return result;
-    } else {
-      logline(chalk.red('Failed to create template'));
-      logline(JSON.stringify(await response.json()));
-      return null;
-    }
+    
+    return null;
   };
 
   const UpdateTemplate = async (template: Template): Promise<Template | null> => {
-    if (!serviceUrl) {
-      logline(chalk.red('Service URL not set, re-run auth command'));
-      return null;
+    try {
+      const response: Response = await baseService.Put(`templates/${template.ref}`, JSON.stringify(template));
+
+      if (response.ok) {
+        const result: Template = (await response.json()) as Template;
+
+        return result;
+      } else {
+        logline(chalk.red('Failed to update template'));
+        logline(JSON.stringify(await response.json()));
+        return null;
+      }
+    } catch (ex) {
+      logline(chalk.red(ex));
     }
 
-    if (!credentials) {
-      logline(chalk.red('You must run the auth command first to initialize the CLI'));
-      return null;
-    }
-
-    let servicePath = `https://${serviceUrl}/v3/templates/${template.ref}`;
-
-    const response: Response = await fetch(servicePath, {
-      method: 'put',
-      body: JSON.stringify(template),
-      headers: {
-        Authorization: `Bearer ${credentials.access_token}`,
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.ok) {
-      const result: Template = (await response.json()) as Template;
-
-      return result;
-    } else {
-      logline(chalk.red('Failed to update template'));
-      logline(JSON.stringify(await response.json()));
-      return null;
-    }
+    return null;
   };
 
   return {
